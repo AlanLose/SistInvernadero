c870e8fc2b7adba72efa831ef5bcb40a6379864c
Alan Lopez-Segovia <alanlopezsegovia@gmail.com>
2025-01-23 10:51:36 -0600

    Agrega Funcion Basica (Humedad + Control Valvula)

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include "stdio.h"
+#include "SensorHumedad1.h"
+#include "Valve1.h"
 
+#define SETPOINT 60u //porcentaje
 int main(void)
 {
-    printf("Hola Mundo\n");
-    return ;
+    int Humidity = 0;
+    printf("Initializing System...\n");
+    (void)hSensorInit();
+    (void)valveInit();
+
+    Humidity = hGetHumidity();
+
+    if(Humidity < SETPOINT)
+    {
+        printf("Valve activated....\n");
+        valveSet(1);
+    }else{
+        /*nothing to do*/
+    }
+
+    return 0;
 }
