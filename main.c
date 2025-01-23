277dddc3fcd415723f6a852731b247a65e77465e
Alan Lopez-Segovia <alanlopezsegovia@gmail.com>
2025-01-23 12:09:26 -0600

    Agrega Funcionalidad Premium

--- main.c
+++ main.c
@@ -1,21 +1,31 @@
 #include "stdio.h"
 #include "SensorHumedad1.h"
 #include "Valve1.h"
+#include "SensorTemp1.h"
+#include "ACFunction1.h"
 
-#define SETPOINT 60u //porcentaje
+#define HSETPOINT 60u //porcentaje
+#define TSETPOINT 30 //grados_celsius
+
 int main(void)
 {
+    int Temperature = 0;
     int Humidity = 0;
     printf("Initializing System...\n");
     (void)hSensorInit();
+    (void)tSensorInit();
     (void)valveInit();
+    (void)acInit();
 
     Humidity = hGetHumidity();
+    Temperature = tGetTemperature();
 
-    if(Humidity < SETPOINT)
+    if ( (Humidity < HSETPOINT) || (Temperature > TSETPOINT)   )
     {
         printf("Valve activated....\n");
         valveSet(1);
+        printf("AC activated...\n");
+        acSet(1);
     }else{
         /*nothing to do*/
     }
