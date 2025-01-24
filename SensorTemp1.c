4cbfc11158bb0b28057714793f4333f4bc9494ae
AlanLose <alanlopezsegovia@gmail.com>
2025-01-24 13:26:15 -0600

    Modifica GetFunc en el archivo SensorTemp.c, algo mas (#2)

--- SensorTemp1.c
+++ SensorTemp1.c
@@ -15,6 +15,11 @@ int tGetTemperature(void)
     int mValue = 40;
     printf("Get Temperature from sensor1 = %d\n", mValue);
     /* Implemantacion para obtener temperatura del sensor 1*/
+    /*Modificacion 1*/
+    /*Modificacion 1*/
+    /*Modificacion 1*/
+    /*Modificacion 1*/
+    /*Modificacion 1*/
     /*Fix1*/
     /*Fix1*/
     return mValue;
